b2c5a855431eb0e576481ce852f0d6eccf7cbc3c
bram <bram@pop-os.localdomain>
2020-07-14 14:43:04 +0200

    init script

--- create_dataset_lists.py
+++ create_dataset_lists.py
@@ -5,9 +5,18 @@ import numpy as np
 
 random.seed(1234)
 
-data_dir = 'data/VCTK/'
+data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'VCTK')
+print(data_dir)
+
 # pathlist = glob.glob(data_dir+"*.spec.npy") # why doesn't this work?
-pathlist = [os.path.join(root, fname) for root, _, fnames in os.walk('data/VCTK/') for fname in fnames if fname[-9:] == ".spec.npy" ]
+pathlist = [os.path.join(root, fname) for root, _, fnames in os.walk(data_dir) for fname in fnames if fname[-9:] == ".spec.npy" ]
+
+# filter out bad items
+pathlist = [path for path in pathlist if \
+    os.path.exists(path.replace(".spec", ".mel")) and \
+    os.path.exists(path.replace("wav48", "txt").replace(".spec.npy", ".txt")) and \
+    os.path.exists(path.replace("wav48", "txt").replace(".spec.npy", ".phones")) \
+]
 
 random.shuffle(pathlist)
 
