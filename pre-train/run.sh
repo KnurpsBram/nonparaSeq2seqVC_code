930f38a4f46e68e658b6398818f2ea30c1697da6
bram <bram@pop-os.localdomain>
2020-07-24 08:35:40 +0200

    code for continue from last checkpoint (with higher iters_per_checkpoint) to prevent full disk space on aws

--- pre-train/run.sh
+++ pre-train/run.sh
@@ -7,9 +7,11 @@
 CUDA_VISIBLE_DEVICES=0 python train.py \
 -l logdir \
 -o ../runs/outdir_embeds_by_resemblyzer_23jul2020 \
+-c ../runs/outdir_embeds_by_resemblyzer_23jul2020/checkpoint_51000 \
 --n_gpus=1 \
 --hparams=speaker_adversial_loss_w=20.,\
 ce_loss=False,\
 speaker_classifier_loss_w=0.1,\
 contrastive_loss_w=30.,\
+iters_per_checkpoint=10000,\
 batch_size=32
