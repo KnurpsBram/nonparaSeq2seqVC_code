f32a73872f2d6c3001fc036d741e9d2be6fe5c4e
bram <bram@pop-os.localdomain>
2020-08-05 14:40:48 +0200

    pickup where leftoff in bashscript

--- pre-train/run.sh
+++ pre-train/run.sh
@@ -7,11 +7,12 @@
 CUDA_VISIBLE_DEVICES=0 python train.py \
 -l logdir \
 -o ../runs/outdir_librispeech_perfile_embeds_by_resemblyzer_24jul2020 \
+-c ../runs/outdir_librispeech_perfile_embeds_by_resemblyzer_24jul2020/checkpoint_80000 \
 --n_gpus=1 \
 --hparams=speaker_adversial_loss_w=20.,\
 ce_loss=False,\
 speaker_classifier_loss_w=0.1,\
 contrastive_loss_w=30.,\
 iters_per_checkpoint=10000,\
-batch_size=2,\
+batch_size=16,\
 epochs=1000
